08cd016d2306fdbb9060efd7ec5e164d4dac735f
TawinNacasit <154253391+TawinNacasit@users.noreply.github.com>
2024-08-01 13:13:22 +0700

    First commit

---
+++ basic-javafx/src/main/java/ku/cs/controllers/HelloController.java
@@ -0,0 +1,29 @@
+package ku.cs.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import ku.cs.services.FXRouter;
+import java.io.IOException;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+    @FXML private ImageView sampleImageView;
+
+    @FXML
+    public void initialize() {
+        Image image = new Image(getClass().getResource("/images/cat.jpg").toString());
+        sampleImageView.setImage(image);
+    }
+
+    @FXML
+    protected void onHelloButtonClick() {
+        try {
+            FXRouter.goTo("student-list");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ basic-javafx/src/main/java/ku/cs/services/Datasource.java
@@ -0,0 +1,8 @@
+package ku.cs.services;
+
+import ku.cs.models.StudentList;
+
+public interface Datasource<T> {
+    T readData();
+    void writeData(T data);
+}
